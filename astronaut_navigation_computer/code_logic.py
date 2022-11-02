251e3998e72788c9f4cafd8395b030e155fd6ba2
miriess <micha.riess@gmail.com>
2022-11-02 23:09:02 +0100

    implement code logic

---
+++ astronaut_navigation_computer/code_logic.py
@@ -0,0 +1,51 @@
+from led_bar import set_all, set_pixel, show
+from time import sleep
+
+flash_frequency = 10
+flash_count = 10
+
+
+def handle_user_input(inputstring, valid_strings_list):
+    if any([x.startswith(inputstring) for x in valid_strings_list]):
+        if inputstring.endswith('#'):
+            success()
+            return inputstring, True
+        else:
+            show_result(len(inputstring) - 1)
+            return inputstring, False
+    else:
+        if inputstring.endswith('*'):
+            show_result(0)
+            return '*', False
+        else:
+            flash_red()
+            return '', False
+
+def flash_red():
+    for _ in range(flash_count):
+        set_all(255, 0, 0)
+        show()
+        sleep(1 / flash_frequency)
+        set_all(0, 0, 0)
+        show()
+        sleep(1 / flash_frequency)
+
+
+def show_result(length):
+    for i in range(length):
+        set_pixel(2*i, 0, 160, 50)
+        set_pixel(2*i + 1, 0, 160, 50)
+    for i in range(length, 4):
+        set_pixel(2*i, 40, 30, 0)
+        set_pixel(2*i + 1, 40, 30, 0)
+    show()
+
+
+def success():
+    for _ in range(flash_count):
+        set_all(0, 255, 0)
+        show()
+        sleep(1 / flash_frequency)
+        set_all(0, 0, 0)
+        show()
+        sleep(1 / flash_frequency)
--- astronaut_navigation_computer/main.py
+++ astronaut_navigation_computer/main.py
@@ -1,6 +1,32 @@
 from keypad import get_user_input
-from time import sleep
+from led_bar import set_all, show
+from code_logic import handle_user_input
+
+set_all(0, 0, 255, 0.04)
+show()
+
+valid_strings_list = ['*' + x + '#' for x in 
+    [
+        "8276",
+        "2149",
+        "8349"
+    ]
+]
+
+inputstring = ""
+valid_result = False
+
 
 while True:
-    print(get_user_input())
-    sleep(0.5)
+    user_input = get_user_input()
+    print(user_input)
+    inputstring += user_input
+    print(inputstring)
+    inputstring, valid_result = handle_user_input(inputstring, valid_strings_list)
+    if valid_result:
+        valid_strings_list.remove(inputstring)
+        inputstring = ""
+        valid_result = False
+    if len(inputstring) == 0:
+        set_all(0, 0, 255)
+        show()
