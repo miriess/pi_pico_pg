6da5c4d43982c19d9d530b0df13d437b87b97563
miriess <micha.riess@gmail.com>
2022-11-11 21:55:16 +0100

    add random code generator

---
+++ astronaut_navigation_computer/random_codes.py
@@ -0,0 +1,16 @@
+from random import randrange
+import json
+
+codelist = []
+
+for i in range(49):
+    code = '*'
+    for i in range(4):
+        code += str(randrange(0,10,1))
+    code += '#'
+    codelist.append(code)
+
+code_json = {'codes': codelist}
+
+with open('codelist.json', 'w') as outputfile:
+    outputfile.write(json.dumps(code_json, indent=2))
